e2030ea636bdabac875bef982556295e7d2ae8c0
LIUY <liuybak001@outlook.com>
2020-07-23 20:52:22 +0800

    1.6 - 1.7 add list and views, add urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -43,5 +44,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
